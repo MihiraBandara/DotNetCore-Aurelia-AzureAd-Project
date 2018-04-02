0c9b97fa228d04e2a337cbe389bdac64151d918f
MihiraBandara <mihira2u@gmail.com>
2018-04-02 11:09:35 +0530

    update seetings file

--- BookShop/src/BookShop.Web.App/src/settings.ts
+++ BookShop/src/BookShop.Web.App/src/settings.ts
@@ -1,7 +1,7 @@
 let config = {
   service: 'http://localhost:59652/api/',
-  clientId: '3e0439a9-acce-490b-917c-6363fee06690',
-  authority: 'https://login.microsoftonline.com/tfp/mihiratest.onmicrosoft.com/B2C_1_signupin'
+  clientId: '***************************************',
+  authority: '*************************************************************'
 };
 
 export default config;
